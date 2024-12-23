dc90888132afc272fee27fe172c5df8497fc60c0
Cinta-i29 <2017151096@qq.com>
2024-12-23 16:59:18 +0800

    test

--- src/main/java/com/gdou/HumanResourceSystemSpringBootApplication.java
+++ src/main/java/com/gdou/HumanResourceSystemSpringBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.gdou.mapper")
 public class HumanResourceSystemSpringBootApplication {
-
+    // test
     public static void main(String[] args) {
         SpringApplication.run(HumanResourceSystemSpringBootApplication.class, args);
     }
